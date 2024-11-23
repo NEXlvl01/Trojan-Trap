f5a061424dbfd1856b25ef95ec6c0a1d28bf5d31
NEXlvl01 <armanthakur2004@gmail.com>
2024-11-23 18:23:35 +0530

    made Homepage and navbar Responsive

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import vid from '../../images/main video2.mp4';
+import './Home.css';
 
 export default function Home() {
   return (
@@ -11,11 +12,11 @@ export default function Home() {
       </div>
 
       <div className='h-full w-full z-30 absolute flex justify-center items-center'>
-        <div className='flex flex-col gap-14 justify-center items-center'>
-          <div className='text-white text-7xl font-semibold flex gap-4 justify-center'><span>Your Shield Against</span><div className='flex gap-4'><span className='text-[#184b81]'>Cyber</span><span className='text-[#279dd7]'>Threats!</span></div></div>
+        <div id = "headDiv" className='flex flex-col gap-14 justify-center items-center'>
+          <div id = "mainHead" className='text-white text-7xl font-semibold flex gap-4 justify-center items-center'><span className='text-center'>Your Shield Against</span><div className='flex gap-4'><span className='text-[#184b81]'>Cyber</span><span className='text-[#279dd7]'>Threats!</span></div></div>
           <div className='flex flex-col gap-6'>
-            <div className='text-[#b9b9b8] text-center text-2xl'>Welcome to TrojanTrap! Scan and secure your files and URLs with ease.</div>
-            <div className='text-[#b9b9b8] text-center text-2xl'>Enjoy real-time virus detection, instant results, nationwide accessibility, and 24/7 online scanning.</div>
+            <div id = "subHead" className='text-[#b9b9b8] text-center text-2xl'>Welcome to TrojanTrap! Scan and secure your files and URLs with ease.</div>
+            <div id = "subHead" className='text-[#b9b9b8] text-center text-2xl'>Enjoy real-time virus detection, instant results, nationwide accessibility, and 24/7 online scanning.</div>
           </div>
           <button className='text-white p-3 bg-[#279dd7] w-[150px] rounded-lg font-semibold hover:bg-[#184b81] transition-all duration-150'>Scan Virus</button>
         </div>
--- frontend/src/Components/Home/Navbar.jsx
+++ frontend/src/Components/Home/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from "../../images/logo.png";
 import { FaUser } from "react-icons/fa";
+import { IoMdMenu } from "react-icons/io";
+import "./Navbar.css";
 
 export default function Navbar() {
     return (
         <div className='bg-[#f0f0ea] h-[120px] w-full'>
-            <div className='flex items-center justify-around h-full'>
+            <div id = "mainNavDiv" className='flex items-center justify-around h-full'>
+                <div id = "menuDiv" className='hidden'>
+                    <IoMdMenu className='text-3xl text-[#184b81]'/>
+                </div>
                 <div className='cursor-pointer'>
                     <img src={logo} alt="" className='w-[220px]' />
                 </div>
-                <div className='flex w-[20%] justify-around'>
+                <div id = "optionsDiv" className='flex w-[20%] justify-around'>
                     <button className='text-[#184b81] font-semibold hover:text-[#279dd7] transition-all duration-300'>Home</button>
                     <button className='text-[#184b81] font-semibold hover:text-[#279dd7] transition-all duration-300'>Scan Virus</button>
                     <button className='text-[#184b81] font-semibold border-[#184b81] border-2 py-3 px-8 rounded-xl flex gap-2 items-center justify-center hover:bg-[#184b81] hover:text-white transition-all duration-200"'><div><FaUser className='text-[#279dd7]'/>
